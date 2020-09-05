bd8c22f8e0e6cba2e6482075246359ecaefafd89
Chad <chad@pop-os.localdomain>
2020-09-05 16:43:37 -0700

    changed context to pass_environment

--- src/flask_base/blueprints/db_common/cli_cmd.py
+++ src/flask_base/blueprints/db_common/cli_cmd.py
@@ -2,7 +2,7 @@ import click
 from flask import current_app
 from flask.cli import with_appcontext
 
-from flask_base.blueprints.cli_base.cli_cmd import app_group
+from flask_base.blueprints.cli_base.cli_cmd import app_group, pass_environment
 from flask_base.extensions import db as flask_db
 
 
@@ -13,7 +13,15 @@ def db():
 
 @db.command("reset")
 @with_appcontext
-@click.pass_context
+@pass_environment
 def reset(ctx):
     flask_db.drop_all(app=current_app)
     flask_db.create_all(app=current_app)
+
+
+@db.command("seed_data")
+@click.option("-t", "--test_data", is_flag=True, help="Seed test data.")
+@with_appcontext
+@pass_environment
+def seed_data(ctx, test_data=False):
+    pass
