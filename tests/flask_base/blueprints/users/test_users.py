e408831d0bd2b5b6f9fe377f0a0ac370b6d01396
Chad <chad@pop-os.localdomain>
2020-09-06 12:57:53 -0700

    db testing

---
+++ tests/flask_base/blueprints/users/test_users.py
@@ -0,0 +1,8 @@
+import pytest
+
+from flask_base.blueprints.users.models import Role, User
+from flask_base.extensions import db
+
+
+def test_admin_role():
+    pass
--- tests/flask_base/conftest.py
+++ tests/flask_base/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
 from flask_base.app import create_app
+from flask_base.extensions import db as flask_db
 
 # from dotenv import load_dotenv
 
@@ -16,3 +17,13 @@ def app():
 def client(app):
     with app.test_client() as test_client:
         yield test_client
+
+
+@pytest.fixture
+def db(app):
+    with app.app_context():
+        yield flask_db
+        # db.create_all()
+        # yield db
+        # db.drop_all()
+        # db.session.commit()
